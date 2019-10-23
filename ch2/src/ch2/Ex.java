daae9fc34b805371b2bbb8f0bf46b932a87eff16
Akihosi <rudqls706@naver.com>
2019-10-23 11:00:03 +0900

    commit

---
+++ ch2/src/ch2/Ex.java
@@ -0,0 +1,15 @@
+package ch2;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		System.out.println("1" + "2");
+		System.out.println(true + "");
+		System.out.println('A' + 'B');
+		System.out.println('1' + 2);
+		System.out.println('1' + '2');
+		System.out.println('J' + "ava");
+		/* System.out.println(true + null); */
+	}
+
+}
---
+++ ch2/src/ch2/SpecialChar.java
@@ -0,0 +1,15 @@
+package ch2;
+
+public class SpecialChar {
+
+	public static void main(String[] args) {
+		char single = '\''; // single = ''';와 같이 할 수 없다
+		String dblOuote = "\"Hello\""; //큰따옴표를 출력하려면 이렇게 코드를 짜면 된다.
+		String root = "c:\\";
+		
+		System.out.println(single);
+		System.out.println(dblOuote);
+		System.out.println(root);
+	} 
+
+}
