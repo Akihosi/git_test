9078901d67bc4405546a79e9aed4c2effe80d5ef
Akihosi <rudqls706@naver.com>
2019-10-23 11:02:14 +0900

    commit

---
+++ ch3/src/ch3/OperatorEx20.java
@@ -0,0 +1,12 @@
+package ch3;
+
+public class OperatorEx20 {
+
+	public static void main(String[] args) {
+		for(int i=1; i <= 10; i++) {
+			if(i%3==0) { //i가 3으로 나누어 떨어지면 3의 배수이므로 출력한다.
+				System.out.println(i);
+			}
+		}
+}
+}
---
+++ ch3/src/ch3/OperatorEx5.java
@@ -0,0 +1,11 @@
+package ch3;
+
+public class OperatorEx5 {
+
+	public static void main(String[] args) {
+		byte s = 10;
+		System.out.println("S = " + s);
+		System.out.println("~S = " + ~s);
+		System.out.println("~S+1 = " + (~s+1)); 
+}
+}
